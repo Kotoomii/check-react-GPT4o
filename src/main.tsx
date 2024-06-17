0b0191e465a376398161e88e5c6bad37def8fdc4
kerokerokohei <135269005+kerokerokohei@users.noreply.github.com>
2024-06-17 22:55:55 +0900

    feat: eを一つ追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 // aaaaa
 // bbbbb
 // ccccc
 // dd
+// e
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App /> 
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
